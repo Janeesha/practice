ab66ef37251b4b92bab8d8694360a2d0cbaea1c6
unknown <hima.ratikanti@gmail.com>
2017-05-23 14:17:49 +0530

    node practice

---
+++ index.js
@@ -0,0 +1,31 @@
+var fs=require("fs");
+var filepath='./simple.txt';
+
+//fs.writeFileSync(filepath,"hi janeesha");
+// var data=fs.readFileSync(filepath);
+// fs.writeFileSync('./output.txt',"hi janeesha");
+// console.log(data.toString());
+
+//  var data=fs.readFile(filepath,function(err,data){
+// 	if(err)
+//  	{
+// 		throw err;
+//  		console.log(err);
+//  	}
+ 	
+
+
+//  fs.writeFile('./write1.txt',data,function(err,data){
+//  	if(err)
+//  	{
+//  		throw err;
+//  		console.log(err)
+// 	}
+// 	console.log(data)
+//  })
+// })
+
+fs.unlink('./write1.txt',function(err){
+        if(err) return console.log(err);
+        console.log('file deleted successfully');
+   });  
