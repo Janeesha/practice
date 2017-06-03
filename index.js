bb38ea2a2dbec8537a4c9d72eae962a7a363324c
unknown <hima.ratikanti@gmail.com>
2017-06-03 20:04:29 +0530

    imp

--- index.js
+++ index.js
@@ -6,24 +6,23 @@ var filepath='./simple.txt';
 // fs.writeFileSync('./output.txt',"hi janeesha");
 // console.log(data.toString());
 
-//  var data=fs.readFile(filepath,function(err,data){
-// 	if(err)
-//  	{
-// 		throw err;
-//  		console.log(err);
-//  	}
+  var data=fs.readFile(filepath,function(err,data){ 	if(err)
+  	{
+ 		throw err;
+ 		console.log(err);
+ 	}
  	
 
 
-//  fs.writeFile('./write1.txt',data,function(err,data){
-//  	if(err)
-//  	{
-//  		throw err;
-//  		console.log(err)
-// 	}
-// 	console.log(data)
-//  })
-// })
+  fs.writeFile('./write1.txt',data,function(err,data){
+  	if(err)
+ 	{
+ 		throw err;
+ 		console.log(err)
+ 	}
+	console.log(data)
+ })
+ })
 
 fs.unlink('./write1.txt',function(err){
         if(err) return console.log(err);
