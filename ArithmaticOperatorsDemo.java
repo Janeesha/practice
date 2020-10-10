44739635e5433c0cea5ff1a0801b512fde71cac5
Janeesha <asha.mp89@gmail.com>
2020-10-10 23:11:27 +0530

    Add files via upload

---
+++ ArithmaticOperatorsDemo.java
@@ -0,0 +1,23 @@
+class ArithmaticOperatorsDemo{
+
+public static void main(String []args){
+
+float a = 75932.157101111111111111111111111236226262626f,b = 20.235115111111111111513626333333333333333f , c = 0f;
+
+c = a+b;
+System.out.println("the value of (c = a+b) is = :" + c);
+
+c = a-b;
+System.out.println("the value of (c = b-a) is = :" + c);
+
+c = a*b;
+System.out.println("the value of (c = a*b) is = :" + c);
+
+c = a/b;
+System.out.println("the value of (c = a/b) is = :" + c);
+
+c = a%b;
+System.out.println("the value of (c = a%b) is = :" + c);
+}
+
+}
